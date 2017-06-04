927e86aaf45ed9cfdc987b9a230a36533c0b460d
Jason Ganub <jasonganub@gmail.com>
2017-06-04 13:22:42 -0700

    Created Vehicle model

--- vehicles/models.py
+++ vehicles/models.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Vehicle(models.Model):
+    height = models.DecimalField(default=0)
+    diameter = models.DecimalField(default=0)
+    mass = models.IntegerField(default=0)
+    stages = models.IntegerField(default=0)
+    payload_to_leo = models.IntegerField(default=0)
+    payload_to_gto = models.IntegerField(default=0)
+    payload_to_mars = models.IntegerField(default=0)
