a317ef44a8f840f9b9298b5229ee5ceaea677ff3
Jason Ganub <jasonganub@gmail.com>
2017-06-04 03:09:39 -0700

    Created vehicles app and updated all areas

--- vehicles/apps.py
+++ vehicles/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class RocketsConfig(AppConfig):
-    name = 'rockets'
+class VehiclesConfig(AppConfig):
+    name = 'vehicles'
