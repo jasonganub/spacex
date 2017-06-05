b033af46b270755a2a311417f4369841a430661b
Jason Ganub <jasonganub@gmail.com>
2017-06-04 17:50:46 -0700

    Added name to Vehicle and dynamic html to vehicles.html

--- vehicles/models.py
+++ vehicles/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 
 class Vehicle(models.Model):
+    name = models.CharField(max_length=100)
     height = models.FloatField(default=0)
     diameter = models.FloatField(default=0)
     mass = models.IntegerField(default=0)
@@ -12,3 +13,6 @@ class Vehicle(models.Model):
     payload_to_leo = models.IntegerField(default=0)
     payload_to_gto = models.IntegerField(default=0)
     payload_to_mars = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
