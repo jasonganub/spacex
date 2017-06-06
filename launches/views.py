b69932c09ea449bad419fd14477cdcdb53a937dc
Jason Ganub <jasonganub@gmail.com>
2017-06-05 21:23:29 -0700

    Created display_all_launches view function for Launch

--- launches/views.py
+++ launches/views.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Launch
 
-# Create your views here.
+
+def display_all_launches(request):
+    if request.method == 'GET':
+        launches = Launch.objects.all()
+        return render(request, 'launches.html', {'launches': launches})
