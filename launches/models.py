9f99eff92438cfe14f846fa8c99b12b90214b0b3
Jason Ganub <jasonganub@gmail.com>
2017-06-05 21:20:27 -0700

    Created the Launch model

--- launches/models.py
+++ launches/models.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Launch(models.Model):
+    name = models.CharField(max_length=100)
+    vehicle = models.CharField(max_length=100)
+    date = models.DateTimeField("date launched")
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
